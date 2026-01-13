a7bbc6d57cf3d4a89bfc089f5f3315daf1199cf3
Seif Ewais <seif.ahmed@milango.app>
2026-01-13 16:11:11 +0200

    tester 3

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,27 +5,23 @@ import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <ThemedText>Welcome message</ThemedText>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flexDirection: 'column',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: '10%',
+    paddingVertical: '20%',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+
   },
 });
